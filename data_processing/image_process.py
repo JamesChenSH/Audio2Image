1a5c03077f3f2b1447b02f7f97cfac40ba39308f
ifEricReturnTrue <eric.z1@outlook.com>
2024-11-10 13:06:20 -0500

    image tensor saved

--- data_processing/image_process.py
+++ data_processing/image_process.py
@@ -1,5 +1,8 @@
 from PIL import Image
 import os
+import torch
+import torchvision.transforms as transforms
+
 
 def resize_image(path, target_path, file_name, factor):
     image = Image.open(path)
@@ -19,6 +22,21 @@ def convert_to_greyscale_image(path, target_path, file_name):
 
     target_file = os.path.join(target_path, file_name)
     image.save(target_file, optimize=True, quality=100)
+
+    return
+
+
+def convert_to_tensor(path, target_path, file_name):
+    
+    image = Image.open(path)
+
+    transform = transforms.ToTensor()
+    image_tensor = transform(image)
+    image_tensor = image_tensor.squeeze(dim=0)
+
+    new_filename = os.path.splitext(file_name)[0] + ".pt"
+    target_file = os.path.join(target_path, new_filename)
+    torch.save(image_tensor, target_file)
 
     return
 
@@ -63,4 +81,25 @@ def convert_greyscale():
             count += 1
     print(count)
 
-convert_greyscale()
+def gs_to_tensor():
+    root_dir = "C:/Users/EricZ/OneDrive/Documents/GitHub/Audio2Image/data/vision_gs"
+    target_dir = "C:/Users/EricZ/OneDrive/Documents/GitHub/Audio2Image/data/vision_gs_tensor"
+
+    count = 0
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        current_folder = os.path.basename(dirpath)
+        curr_target_dir = os.path.join(target_dir, current_folder)
+        if not os.path.exists(curr_target_dir):
+            os.makedirs(curr_target_dir)
+        for filename in filenames:
+            if filename == ".DS_Store":
+                continue
+            file_path = os.path.join(dirpath, filename)
+
+            convert_to_tensor(file_path, curr_target_dir, filename)
+            
+            count += 1
+
+    print(count)
+
+gs_to_tensor()
