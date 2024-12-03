d32956f74b692ab6e82ae49978e531b2406d459d
Jinyang (Jerry) Zhao <jerryzhao1025@gmail.com>
2024-12-03 17:50:39 -0500

    modifit overlap rate

--- data_processing/sound_process.py
+++ data_processing/sound_process.py
@@ -56,7 +56,8 @@ def process_audio_files(root_dir, target_dir):
                 # split data into 10 pieces
                 PIECE_NUM = 50
                 segment_length = len(data) // PIECE_NUM
-                gap_between_two_segments = int(0.5 * segment_length)
+                OVERLAP_RATE = 0.5
+                gap_between_two_segments = int((1 - OVERLAP_RATE) * segment_length)
                 fft_data_segments = []
 
                 idx = 0
@@ -127,7 +128,7 @@ if __name__ == "__main__":
     
     sound_root_dir = os.path.join(dataset_folder, "sound")
     processed_dir = os.path.join(dataset_folder, "audio_processed")
-    process_audio_files(sound_root_dir, processed_dir)
+    # process_audio_files(sound_root_dir, processed_dir)
 
     # process_audio_files(sound_root_dir, processed_dir)
     joint_dir = os.path.join(dataset_folder, "audio_airport_tensor.pt")
